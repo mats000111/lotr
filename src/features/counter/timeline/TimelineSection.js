ca8192cf6907833ba5349dbcb0aaa963f67b0986
Mats Ribaucourt <mats-ribaucourt@hotmail.com>
2023-03-04 17:12:50 +0700

    partially build timeline modal

--- src/features/counter/timeline/TimelineSection.js
+++ src/features/counter/timeline/TimelineSection.js
@@ -1,7 +1,12 @@
-import { Col, Row, Player } from "reactstrap";
+import { useState } from "react";
+import { Col, Row, Modal, ModalHeader, ModalBody, ModalFooter, Button } from "reactstrap";
+import { Player } from "video-react";
 
 const TimelineSection = ({ event }) => {
     const { name, date, description, image, video } = event;
+    const [modal, setModal] = useState(false);
+
+    const toggle = () => setModal(!modal)
 
     return (
         <Row>
@@ -14,6 +19,24 @@ const TimelineSection = ({ event }) => {
             <Col>
                 {image}
             </Col>
+            <Col className="col-1">
+                <Button>add previous section</Button>
+                <Button onClick={toggle}>Details</Button>
+                <Button>add next section</Button>
+            </Col>
+            <Modal isOpen={modal}>
+                <ModalHeader toggle={() => setModal(false)}>{name}</ModalHeader>
+                <ModalBody>
+                    <div>
+                        <Player>
+                            <source src={video}/>
+                        </Player>
+                    </div>
+                    <div>
+                        {description}
+                    </div>
+                </ModalBody>
+            </Modal>
         </Row>
     );
 };
